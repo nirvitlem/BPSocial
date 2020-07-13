334e766633eb1c9dd6108a71c6cd620b2eb24513
NirV <nvitlem@gmail.com>
2020-07-13 13:08:04 +0300

    add thread for write

--- app/src/main/java/com/NV/bpsocial/MyBluetoothService.kt
+++ app/src/main/java/com/NV/bpsocial/MyBluetoothService.kt
@@ -424,9 +424,10 @@ class MyBluetoothService(private val mmSocket: BluetoothSocket) : Thread() {
     fun write(bytes: ByteArray) {
         try {
             if (logEnable) Log.e(TAG, "mmOutStream.write(bytes)");
-            mmOutStream.write(bytes)
-            mmOutStream.flush();
-           // mmBuffer = ByteArray(bufferSize)
+            Thread({
+                mmOutStream.write(bytes)
+                mmOutStream.flush();
+            }).start()
         } catch (e: IOException) {
             if (logEnable) Log.e(TAG, "Error occurred when sending data", e)
             Objectlist.MBSArray?.remove(mmSocket);
